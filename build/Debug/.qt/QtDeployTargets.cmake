96bb542ea1eef27f6be9573f30a56002b79bfd0b
wangzilei0601 <3389018640@qq.com>
2026-07-14 19:24:43 +0800

    停止跟踪build编译目录

--- build/Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Calculator_FILE D:/school/freshman3/cpp_practice/first/build/Debug/Calculator.exe)
-set(__QT_DEPLOY_TARGET_Calculator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Calculator_RUNTIME_DLLS D:/tools/Qt/6.11.1/mingw_64/bin/Qt6Widgets.dll;D:/tools/Qt/6.11.1/mingw_64/bin/Qt6Gui.dll;D:/tools/Qt/6.11.1/mingw_64/bin/Qt6Core.dll)
